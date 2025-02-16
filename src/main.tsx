117573c266f5d2d2051abc8948bee0017b5ede04
cara <cara656513@gmail.com>
2025-02-16 18:49:16 +0900

    sentry 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://6d94f52ca08bf614eb1551432968a5e9@o4508691836305408.ingest.us.sentry.io/4508828203941888",
+  integrations: [],
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
